132ea1116140cd78c3f6f8a35729028ecf2f634f
Rakesh-sr5 <169175249+Rakesh-sr5@users.noreply.github.com>
2025-09-08 14:32:14 +0530

    Create main.py
    parsed json file and created a database

---
+++ main.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""securin assessment
+
+Original file is located at
+    https://colab.research.google.com/drive/1LtGEAlDXJPklmDC3pbWflHWWOpGh_zsA
+"""
+
+# Parsing the file
+import json
+import numpy
+import pandas
+File = open('/content/Json file/US_recipes_null.Pdf.json')
+
+recipes = json.load(File)
+df = pandas.DataFrame(recipes)
+df.head()
+
+df = df.transpose()
+df.head()
+
+#finding Nan values
+df.isna().sum()
+
+#Replacing Nan values with NULL
+df.replace(numpy.nan,'NULL',inplace=True)
+df.isna().sum()
+
+#creating a database
+import sqlite3
+c = sqlite3.connect('s.db')
+cursor = c.cursor()
+q1 = '''CREATE TABLE recipes(
+  cuisine TEXT,
+  title VARCHAR,
+  rating FLOAT,
+   prep_time INT,
+   cook_time INT,
+    total_time INT,
+     description TEXT,
+serves VARCHAR);
+'''
+
+cursor.execute(q1)
+print("THE DATABASE IS SUCCESSFULLY CREATED")
+
+new_df = df[['cuisine','title','rating','prep_time','cook_time','total_time','description','serves']]
+new_df
+
+for row in new_df.values:
+  cursor.execute("INSERT INTO recipes VALUES(?,?,?,?,?,?,?,?)", row)
+
+cursor.execute("SELECT * FROM recipes")
+cursor.fetchall()
+
